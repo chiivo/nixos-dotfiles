7689967cea007b3fe96061fd785b654241e186e9
Sal <sssesmas2@gmail.com>
2021-07-12 18:20:52 -0700

    updated 7/12/21

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+xrandr --output DP-2 --mode 1920x1080 --rate 144.00 --output HDMI-0 --mode 1920x1080 --rate 60.00 &
 nitrogen --restore &
-picom &
+picom --experimental-backends &
 udiskie &
 discord &
+blueman-applet &
