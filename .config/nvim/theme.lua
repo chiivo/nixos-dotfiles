88b3814526f179bb42c3e7fcc6a109fd10e6c24d
Sal <sssesmas2@gmail.com>
2021-09-13 10:48:17 -0700

    Create theme.lua

---
+++ .config/nvim/theme.lua
@@ -0,0 +1,17 @@
+local theme = {}
+
+theme.default = {
+  bg = '#262727',
+  fg = '#f5d1c8',
+  yellow = '#eadc84',
+  cyan = '#77bee0',
+  darkblue = '#77bee0',
+  green = '#bde077',
+  orange = '#ffc178',
+  violet = '#dd91f3',
+  magenta = '#dd91f3',
+  blue = '#77bee0';
+  red = '#ec5f67';
+}
+
+return theme
