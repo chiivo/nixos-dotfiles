fb4ba9d8c9e6112fff1ee1297c82a8e60a7d4d00
Sal <sssesmas2@gmail.com>
2021-11-27 16:25:17 -0800

    Update bliss.lua

--- .config/nvim/colors/bliss/lua/lush_theme/bliss.lua
+++ .config/nvim/colors/bliss/lua/lush_theme/bliss.lua
@@ -114,7 +114,7 @@ local theme = lush(function()
     -- Question     { }, -- |hit-enter| prompt and yes/no questions
     QuickFixLine {bg = yellow}, -- Current |quickfix| item in the quickfix window. Combined with |hl-CursorLine| when the cursor is there.
     Search       {bg = yellow}, -- Last search pattern highlighting (see 'hlsearch').  Also used for similar items that need to stand out.
-    -- SpecialKey   { }, -- Unprintable characters: text displayed differently from what it really is.  But not 'listchars' whitespace. |hl-Whitespace|
+    SpecialKey   {fg = orange}, -- Unprintable characters: text displayed differently from what it really is.  But not 'listchars' whitespace. |hl-Whitespace|
     -- SpellBad     { }, -- Word that is not recognized by the spellchecker. |spell| Combined with the highlighting used otherwise. 
     -- SpellCap     { }, -- Word that should start with a capital. |spell| Combined with the highlighting used otherwise.
     -- SpellLocal   { }, -- Word that is recognized by the spellchecker as one that is used in another region. |spell| Combined with the highlighting used otherwise.
