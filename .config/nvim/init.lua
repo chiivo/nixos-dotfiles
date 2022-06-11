e5ea67871c2025b4f603bc0d05cbed2697380c0d
Sal <sssesmas2@gmail.com>
2022-06-10 21:52:49 -0700

    Add files via upload

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -740,13 +740,13 @@ require('orgmode').setup_ts_grammar()
 -- Tree-sitter configuration
 require'nvim-treesitter.configs'.setup {
   -- If TS highlights are not enabled at all, or disabled via `disable` prop, highlighting will fallback to default Vim syntax highlighting
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = {'org'}, -- Required for spellcheck, some LaTex highlights and code block highlights that do not have ts grammar
-  },
-  ensure_installed = {'org'}, -- Or run :TSUpdate org
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = {'org'}, -- Required for spellcheck, some LaTex highlights and code block highlights that do not have ts grammar
+	},
+	ensure_installed = {'org'}, -- Or run :TSUpdate org
 }
 require('orgmode').setup({
-  org_agenda_files = {'~/Dropbox/org/*', '~/my-orgs/**/*'},
-  org_default_notes_file = '~/Dropbox/org/refile.org',
+	org_agenda_files = {'~/Dropbox/org/*', '~/my-orgs/**/*'},
+	org_default_notes_file = '~/Dropbox/org/refile.org',
 })
