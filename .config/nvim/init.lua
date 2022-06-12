994cc5f25b35dded7d6a89bf7c687e983b2525e3
Sal <sssesmas2@gmail.com>
2022-06-11 23:34:57 -0700

    Add files via upload

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -70,46 +70,63 @@ require'colorizer'.setup({
 })
 
 --Dashboard
+local home = os.getenv('HOME')
+local db = require('dashboard')
+db.custom_header = {
+	'',
+	'',
+	' ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗',
+	' ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║',
+	' ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║',
+	' ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║',
+	' ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║',
+	' ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝',
+	'',
+	'',
+}
+db.custom_center = {
+	{
+		icon = '  ',
+		desc = 'Last session					',
+		shortcut = 'SPC s l',
+		action ='SessionLoad'
+	},
+	{
+		icon = '  ',
+		desc = 'Recently opened files					',
+		action =  'DashboardFindHistory',
+		shortcut = 'SPC f h'
+	},
+	{
+		icon = '  ',
+		desc = 'Find  File					',
+		action = 'Telescope find_files find_command=rg,--hidden,--files',
+		shortcut = 'SPC f f'
+	},
+	{
+		icon = '  ',
+		desc ='File Browser					',
+		action =  'Telescope file_browser',
+		shortcut = 'SPC f b'
+	},
+	{
+		icon = '  ',
+		desc = 'Find word					',
+		aciton = 'DashboardFindWord',
+		shortcut = 'SPC f w'
+	},
+	{
+		icon = '  ',
+		desc = 'Open Dotfiles					',
+		action = 'Telescope dotfiles path=' .. home ..'/.dotfiles',
+		shortcut = 'SPC f d'
+	},
+}
 vim.cmd([[
-let g:dashboard_default_executive ='telescope'
-let g:dashboard_custom_shortcut={
-\ 'last_session'       : 'SPC s l',
-\ 'find_history'       : 'SPC f h',
-\ 'find_file'          : 'SPC f f',
-\ 'new_file'           : 'SPC c n',
-\ 'change_colorscheme' : 'SPC t c',
-\ 'find_word'          : 'SPC f a',
-\ 'book_marks'         : 'SPC f b',
-\ }
-let g:dashboard_custom_shortcut_icon={
-\ 'last_session'				: ' ',
-\ 'find_history'				: ' ',
-\ 'find_file'						: ' ',
-\ 'new_file'						: ' ',
-\ 'change_colorscheme'	: ' ',
-\ 'find_word'						: ' ',
-\ 'book_marks'					: ' ',
-\ }
-nmap <Leader>ss :<C-u>SessionSave<CR>
-nmap <Leader>sl :<C-u>SessionLoad<CR>
-nnoremap <silent> <Leader>fh :DashboardFindHistory<CR>
-nnoremap <silent> <Leader>ff :DashboardFindFile<CR>
-nnoremap <silent> <Leader>tc :DashboardChangeColorscheme<CR>
-nnoremap <silent> <Leader>fa :DashboardFindWord<CR>
-nnoremap <silent> <Leader>fb :DashboardJumpMark<CR>
-nnoremap <silent> <Leader>cn :DashboardNewFile<CR>
 highlight DashboardHeader guifg = '#f5d1c8'
 highlight DashboardCenter guifg = '#77bee0'
 highlight DashboardShortcut guifg = '#ff8278'
 highlight DashboardFooter guifg = '#dd91f3'
-let g:dashboard_custom_header = [
-\ ' ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗',
-\ ' ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║',
-\ ' ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║',
-\ ' ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║',
-\ ' ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║',
-\ ' ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝',
-\]
 ]])
 
 --Bufferline
