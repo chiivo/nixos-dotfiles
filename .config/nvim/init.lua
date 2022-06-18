2bcbf480be9365b6fcff9442292d8009d7b89ebe
Sal <sssesmas2@gmail.com>
2022-06-17 19:16:06 -0700

    Add files via upload

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -22,6 +22,13 @@ require('packer').startup(function()
 	use 'folke/twilight.nvim'
 	use 'nvim-orgmode/orgmode'
 	use 'folke/which-key.nvim'
+	use 'hrsh7th/cmp-buffer'
+	use 'hrsh7th/cmp-path'
+	use 'hrsh7th/cmp-cmdline'
+	use 'hrsh7th/nvim-cmp'
+	use 'L3MON4D3/LuaSnip'
+	use 'saadparwaiz1/cmp_luasnip'
+	use 'rafamadriz/friendly-snippets'
 end)
 
 --Colors
@@ -762,3 +769,53 @@ require('orgmode').setup({
 
 --Which-Key
 require("which-key").setup{}
+
+--Nvim-Cmp
+local cmp = require'cmp'
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-k>'] = cmp.mapping.select_prev_item(),
+		['<C-j>'] = cmp.mapping.select_next_item(),
+		['<C-b>'] = cmp.mapping.scroll_docs(-1),
+		['<C-f>'] = cmp.mapping.scroll_docs(1),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	}),
+	sources = cmp.config.sources({
+		{ name = 'luasnip' }, -- For luasnip users.
+		{ name = 'buffer' },
+		{ name = 'path'},
+	})
+})
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+	sources = cmp.config.sources{
+		{ name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+	},
+	{
+		{ name = 'buffer' },
+	}
+})
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline('/', {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = 'buffer' }
+	}
+})
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = 'path' }
+	},
+	{
+		{ name = 'cmdline' }
+	})
+})
