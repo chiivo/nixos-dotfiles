0f10d48a468c61d08229ff4aa0608cb82874579b
Sal <sssesmas2@gmail.com>
2022-06-17 23:00:13 -0700

    Add files via upload

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -771,14 +771,41 @@ require('orgmode').setup({
 require("which-key").setup{}
 
 --Nvim-Cmp
+local kind_icons = {
+  Text = " ",
+  Method = "m",
+  Function = " ",
+  Constructor = " ",
+  Field = "",
+  Variable = " ",
+  Class = " ",
+  Interface = " ",
+  Module = " ",
+  Property = " ",
+  Unit = " ",
+  Value = " ",
+  Enum = " ",
+  Keyword = " ",
+  Snippet = " ",
+  Color = " ",
+  File = " ",
+  Reference = "*",
+  Folder = " ",
+  EnumMember = " ",
+  Constant = " ",
+  Struct = " ",
+  Event = " ",
+  Operator = " ",
+  TypeParameter = " ",
+}
 local cmp = require'cmp'
-cmp.setup({
+cmp.setup{
 	snippet = {
 		expand = function(args)
 			require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
 		end,
 	},
-	mapping = cmp.mapping.preset.insert({
+	mapping = {
 		['<C-k>'] = cmp.mapping.select_prev_item(),
 		['<C-j>'] = cmp.mapping.select_next_item(),
 		['<C-b>'] = cmp.mapping.scroll_docs(-1),
@@ -786,36 +813,30 @@ cmp.setup({
 		['<C-Space>'] = cmp.mapping.complete(),
 		['<C-e>'] = cmp.mapping.abort(),
 		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-	}),
-	sources = cmp.config.sources({
+	},
+	formatting = {
+		fields = { "kind", "abbr", "menu" },
+		format = function(entry, vim_item)
+			-- Kind icons
+			vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+			-- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+			vim_item.menu = ({
+				nvim_lsp = "[LSP]",
+				luasnip = "[Snippet]",
+				buffer = "[Buffer]",
+				path = "[Path]",
+			})[entry.source.name]
+			return vim_item
+		end,
+	},
+	sources = {
 		{ name = 'luasnip' }, -- For luasnip users.
 		{ name = 'buffer' },
 		{ name = 'path'},
-	})
-})
--- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
-	sources = cmp.config.sources{
-		{ name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
 	},
-	{
-		{ name = 'buffer' },
+	window = {
+		documentation = {
+			border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+		}
 	}
-})
--- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline('/', {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = {
-		{ name = 'buffer' }
-	}
-})
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-	mapping = cmp.mapping.preset.cmdline(),
-	sources = cmp.config.sources({
-		{ name = 'path' }
-	},
-	{
-		{ name = 'cmdline' }
-	})
-})
+}
