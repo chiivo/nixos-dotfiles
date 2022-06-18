faae9c66ed34a48b221872e5ba0f68b023014a26
Sal <sssesmas2@gmail.com>
2022-06-17 23:27:40 -0700

    Add files via upload

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -824,10 +824,10 @@ cmp.setup{
 			vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
 			-- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
 			vim_item.menu = ({
-				nvim_lsp = "[LSP]",
-				luasnip = "[Snippet]",
-				buffer = "[Buffer]",
-				path = "[Path]",
+				nvim_lsp = "(LSP)",
+				luasnip = "(Snippet)",
+				buffer = "(Buffer)",
+				path = "(Path)",
 			})[entry.source.name]
 			return vim_item
 		end,
@@ -839,7 +839,10 @@ cmp.setup{
 	},
 	window = {
 		documentation = {
-			border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+			border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" }
+		},
+		completion = {
+			border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" }
 		}
 	}
 }
