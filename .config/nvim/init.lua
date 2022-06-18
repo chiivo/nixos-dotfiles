e493e45930f253d40e6769ab8b903345d33588ae
Sal <sssesmas2@gmail.com>
2022-06-18 15:15:27 -0700

    Add files via upload

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -846,3 +846,16 @@ cmp.setup{
 		}
 	}
 }
+cmp.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' },
+    { name = 'cmdline' }
+  })
+})
