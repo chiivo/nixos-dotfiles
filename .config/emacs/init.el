e2a0be9fe493532ab1d60d03004adfad74b892f2
Sal <sssesmas2@gmail.com>
2023-03-12 22:47:19 -0700

    Add files via upload

---
+++ .config/emacs/init.el
@@ -0,0 +1,17 @@
+(org-babel-load-file
+	(expand-file-name
+		"config.org"
+		user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("540f3e244bddc8e8934539a8aaa6f32b353306872eb7f73ce789282c01ecbd98" "58b28db036ce44acc018ef77c38b4ca975a07bfec9423ce433bd16854255e658" "5cc128bed3fae020d76ccce9495fab138746fe9448c859eac8bab689d43215d0" "955361ad8c12b0123e6760e4157d88f787ee6cf08d689c94bac394cb953a32b0" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
